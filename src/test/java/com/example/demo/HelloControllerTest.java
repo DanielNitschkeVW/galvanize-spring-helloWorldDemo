ed79328620758932e38b3ce99a964fcff83c80d8
Nitschke, Daniel <nitschke.daniel@volkswagen.de>
2022-02-23 12:19:21 +0100

    implemented hello world with refactor

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void sayHello_noArgs() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    void sayHello_withName() throws Exception {
+        mockMvc.perform((get("/hello?name=Peter")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Peter"));
+    }
+}
